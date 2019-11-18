56f5918192ca1eac0659ebf4ec304b354a4b2417
Shruti Harrison <51241036+ShrutiHarrison@users.noreply.github.com>
2019-11-18 23:00:57 +0530

    Functions Added
    +Counting number of edges and vertices in graph
    +Checking if a graph is valid
    +To count odd and even degree vertices
    +To check if a graph is complete

--- Adjacency Matrix.c
+++ Adjacency Matrix.c
@@ -4,12 +4,16 @@
 #include<stdlib.h>
 int n;
 void create_graph(int**,int);
-
+int count_edges(int**,int);
 void display(int**,int);
+void check_graph(int**,int);
+void complete(int**,int);
+void even_odd(int**,int);
+
 int main()
 { 
-	int n,**adj,i,j;
-	printf("Enter number of nodes : ");
+	int n,e,**adj,i,j;
+	printf("Enter number of vertices : ");
     scanf("%d", &n);
    adj=(int**)calloc(n,sizeof(int));
 	for( i=0;i<n;i++)
@@ -18,13 +22,19 @@ int main()
 		}
     int choice;
     int node, origin, destin;
+    int cycle;
     
     while (1) {
     	   printf("0. Create a graph \n");
         printf("1.BFS\n");
         printf("2.DFS\n");
         printf("3.Display\n");
-        printf("4.Exit\n");
+        printf("4.Count edges \n");
+        printf("6.Check graph\n");
+        printf("7.Check for complete graph\n");
+         printf("8.Count even and odd degree vertices\n");
+        // printf(" 9. detect cycle in graph\n");
+        printf("5.Exit\n");
         printf("Enter your choice : ");
         scanf("%d", &choice);
         switch (choice) 
@@ -36,8 +46,21 @@ int main()
             display(adj,n);
             break;
        case 4:
+           e= count_edges(adj,n);
+		  break;	
+     case 5:
             exit(0); 
-        default:
+     case 6:
+     	check_graph(adj,n);
+		break;
+	case 7:
+		complete(adj,n);
+		break;
+	case 8:
+             even_odd(adj,n);
+		break;
+	
+     default:
             printf("Wrong choice\n");
             break;
         }
@@ -77,5 +100,62 @@ void create_graph(int **adj,int n)
      
         printf("\n");
     }
+}
+int count_edges(int **adj,int n)
+{
+	printf("\n The number of vertices are %d",n);
+	int i,j,e=0;
+	for(i=0;i<n;i++)
+	{
+	   for(j=0;j<n;j++)
+	   {
+	   	if(adj[i][j]==1)
+	   	e=e+1;
+	   }
+	}
+	printf("\n The number of edges are %d \n",e);
+	return e;
+}
+void check_graph(int **adj,int n)
+{
+	int i,edge,sum=0,d;
+	for(i=0;i<n;i++)
+	{
+	printf("\n Enter the degree for node %d  :",i);
+	scanf("%d",&d);
+	sum=sum+d;	
+	}
+	edge=count_edges(adj,n);
+	if(sum== (2*edge))
+		printf("\n Graph is valid \n");
+	else
+	   printf("\n Graph is invalid \n");
+}
+void complete(int **adj,int n)
+{
+	int e;
+	e=count_edges(adj,n);
+	if(e==(n*(n-1)/2))
+	  printf("\n graph is complete \n");
+	else
+	printf("\n graph is incomplete \n");
+}
+void even_odd(int **adj,int n)
+{
+	int i,j,even=0,odd=0,deg=0;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(adj[i][j]==1)
+			deg++;
+		}
+		if(deg%2==0)
+		 even++;
+		 else 
+		 odd++;
+	}
+	printf("\n number of even degree vertices %d",even);
+	printf("\n number of odd degree vertices %d",odd);
 }
 
